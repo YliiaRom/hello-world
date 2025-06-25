7580c35ef0a52ed4f89c225bba3b53e9fc30e384
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2025-06-25 15:22:50 +0300

    modif hero

--- src/components/HeaderModalButton/HeaderModalButton.jsx
+++ src/components/HeaderModalButton/HeaderModalButton.jsx
@@ -13,11 +13,13 @@ function HeaderModalButton({ btnText, openContactForm }) {
           stiffness: 100,
           damping: 15,
         }}
-        className="ml-4 px-4 py-2 rounded-sm bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-black transition-all duration-500"
-        style={{
-          background:
-            "linear-gradient(90deg, transparent, #e4e4e3, #fff, #e4e4e3, transparent",
-        }}
+        // className="ml-4 px-4 py-2 rounded-sm bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-black transition-all duration-500"
+        className="ml-4 px-4 py-2 rounded-md 
+        bg-transparent text-gray-300 font-bold border-2 shadow-[-1px_-1px_2px_#fff,_2px_2px_3px_#000] hover:from-violet-700 hover:to-purple-700 hover:text-black transition-all duration-500"
+        // style={{
+        //   background:
+        //     "linear-gradient(90deg, transparent, #e4e4e3, #fff, #e4e4e3, transparent",
+        // }}
       >
         {btnText}
       </motion.button>
--- src/components/HeroSection/HeroSectionRight.jsx
+++ src/components/HeroSection/HeroSectionRight.jsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import boy02 from "../../assets/img/boy02.png";
+import boy from "../../assets/img/boy.png";
+import girl from "../../assets/img/girl02.png";
 // import Spline from "@splinetool/react-spline";
 // import { useEffect, useState } from "react";
 function HeroSectionRight() {
@@ -19,11 +21,21 @@ function HeroSectionRight() {
         animate={{ opacity: 1 }}
         className="absolute xl:right-[-28%] right-0 bottom-[-10%] lg:bottom-[-10%] flex  justify-center items-center w-full h-full "
       >
-        <div className="max-w-[900px] w-full aspect-[3/4] flex justify-center items-center border border-violet-200">
+        <div className="relative max-w-[600px] w-full min-w-[400px] aspect-[3/4] flex justify-center items-center heroAvatar">
           <img
             src={boy02}
             alt="boy02"
-            className="w-full h-full object-contain"
+            className="absolute inset-0 w-full h-full object-contain"
+          />
+          <img
+            src={girl}
+            alt="boy02"
+            className="absolute inset-0 w-full h-full object-contain"
+          />
+          <img
+            src={boy}
+            alt="boy02"
+            className="absolute inset-0 w-full h-full object-contain"
           />
         </div>
         {/* <div>
